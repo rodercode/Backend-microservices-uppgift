c0350957ff77bb7da952475beaa78facde4c1e54
RoderCode <Rodersnow@gmail.com>
2023-03-03 10:19:47 +0100

    update

--- group-transport/group-transport-api/src/main/java/com/example/grouptransportapi/dao/GuildRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.grouptransportapi.dao;
-import com.example.grouptransportapi.bean.Guild;
-import com.example.grouptransportapi.bean.Trip;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface GuildRepository extends JpaRepository<Guild, Long> {
-    public Guild findByName(String name);
-}
