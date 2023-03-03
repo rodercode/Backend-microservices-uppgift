2f72cd41e264de5ae7130b80104fb686e678e5de
RoderCode <Rodersnow@gmail.com>
2023-03-03 10:12:11 +0100

    update folder

--- vehicle-apiOne/src/main/java/com/example/vehicleapione/handler/UniqueValidationException.java
+++
@@ -1,9 +0,0 @@
-package com.example.vehicleapione.handler;
-public class UniqueValidationException extends RuntimeException {
-    public UniqueValidationException(String message) {
-        super(message);
-    }
-    public UniqueValidationException(Throwable cause) {
-        super(cause);
-    }
-}
